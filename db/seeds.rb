13fe51b317a194a2554fd46f393f1214f3a2c602
Antonio Furtado <antoniordf@hotmail.com>
2022-11-22 11:42:35 +0000

    migrated database in my local env and updated formating

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,18 +9,15 @@
 my_user = User.new(email: "test1@gmail.com", password: "123456")
 my_user.save!
 
-
 my_yacht = Yacht.create(name: "Capri",
-
-                     length: 29,
-                     capacity: 20,
-                     description: "Its a good yacht",
-                     crew: 10,
-                     price_per_day: 50_000,
-                     location: "monaco",
-                     cabin: 10,
-                     speed: 20)
+                        length: 29,
+                        capacity: 20,
+                        description: "Its a good yacht",
+                        crew: 10,
+                        price_per_day: 50_000,
+                        location: "monaco",
+                        cabin: 10,
+                        speed: 20)
 
 my_yacht.user = my_user
 my_yacht.save!
-
