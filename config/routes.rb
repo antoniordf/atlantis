b32e4124e3fb7e1b39f5826b16541e97b0f319e5
Antonio Furtado <antoniordf@hotmail.com>
2022-11-22 17:34:36 +0000

    added a navbar to the show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # get "yachts", to: "yachts#index"
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "yachts#index"
   resources :yachts
 end
