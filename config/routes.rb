ca450b4780d02f30d5b40ec0c436ca98d70d8944
Gaoren Hao <gaorenh@gmail.com>
2022-11-22 16:27:35 +0000

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   # get "yachts", to: "yachts#index"
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :yachts
+  resources :yachts do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :show, :edit, :update, :destroy]
 end
