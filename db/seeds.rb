763c68e90b4d1091da04579a86efa0f17de231dd
Rashid Mursekaev <r.mursekaev@gmail.com>
2022-11-22 16:46:22 +0000

    cards properlly displaying

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-my_user = User.new(email: "test1@gmail.com", password: "123456")
+my_user = User.new(email: "test2@gmail.com", password: "123456")
 my_user.save!
 
 
@@ -24,3 +24,30 @@ my_yacht = Yacht.create(name: "Capri",
 my_yacht.user = my_user
 my_yacht.save!
 
+my_yacht1 = Yacht.create(name: "Capro",
+
+  length: 29,
+  capacity: 20,
+  description: "Its a good yacht",
+  crew: 10,
+  price_per_day: 50_000,
+  location: "monaco",
+  cabin: 10,
+  speed: 20)
+
+my_yacht1.user = my_user
+my_yacht1.save!
+
+my_yacht2 = Yacht.create(name: "Capra",
+
+  length: 29,
+  capacity: 20,
+  description: "Its a good yacht",
+  crew: 10,
+  price_per_day: 50_000,
+  location: "monaco",
+  cabin: 10,
+  speed: 20)
+
+my_yacht2.user = my_user
+my_yacht2.save!
