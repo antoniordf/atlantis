7f30182a708d8181b550d32e1532cac3cd9f8e4b
Gaoren Hao <gaorenh@gmail.com>
2022-11-24 13:45:48 +0000

    partially done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -18,7 +18,7 @@ class BookingsController < ApplicationController
     @booking.user = current_user
     @booking.yacht = @yacht
     if @booking.save
-      redirect_to bookings_path
+      redirect_to booking_path(@booking)
     else
       render :new, status: :unprocessable_entity
     end
